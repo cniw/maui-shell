3af0cfc3a0b4740b6affab45acad8090635a4d73
UriHerrera <hello@uriherrera.com>
2022-02-24 18:34:53 -0600

    add deps

--- travis-build.sh
+++ travis-build.sh
@@ -39,6 +39,7 @@ DEBIAN_FRONTEND=noninteractive apt -qq update
 ### Maui Shell needs ECM > 5.70
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	kinit-dev \
 	libkf5activities-dev \
 	libkf5activitiesstats-dev \
 	libkf5config-dev \
@@ -48,14 +49,19 @@ DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
 	libkf5doctools-dev \
 	libkf5i18n-dev \
 	libkf5idletime-dev \
+	libkf5itemmodels-dev \
 	libkf5kio-dev \
 	libkf5notifications-dev \
 	libkf5notifyconfig-dev \
 	libkf5people-dev \
+	libkf5prison-dev \
 	libkf5runner-dev \
 	libkf5service-dev \
 	libkf5su-dev \
+	libkf5texteditor-dev \
+	libkf5unitconversion-dev \
 	libkf5wallet-dev \
+	libkf5wayland-dev \
 	libqt5svg5-dev \
 	libqt5waylandcompositor5-dev \
 	mauikit-filebrowsing-git \
@@ -118,7 +124,7 @@ checkinstall -D -y \
 	--pakdir=../.. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=maui-shell-git \
-	--requires="libc6,libkf5activities5,libkf5activitiesstats5,libkf5configcore5,libkf5coreaddons5,libkf5crash5,libkf5declarative5,libkf5doctools5,libkf5i18n5,libkf5idletime5,libkf5kiocore5,libkf5notifications5,libkf5notifyconfig5,libkf5people5,libkf5runner5,libkf5service5,libkf5su5,libkf5wallet5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5svg5,libqt5waylandcompositor5,libqt5widgets5,libstdc++6,mauikit-filebrowsing-git \(\>= 2.1.1+git\),mauikit-git \(\>= 2.1.1+git\),qml-module-qt-labs-calendar,qml-module-qt-labs-platform" \
+	--requires="kinit,libc6,libkf5activities5,libkf5activitiesstats5,libkf5configcore5,libkf5coreaddons5,libkf5crash5,libkf5declarative5,libkf5doctools5,libkf5i18n5,libkf5idletime5,libkf5itemmodels5,libkf5kiocore5,libkf5notifications5,libkf5notifyconfig5,libkf5people5,libkf5prison5,libkf5runner5,libkf5service5,libkf5su5,libkf5texteditor,libkf5unitconversion5,libkf5wallet5,libkf5wayland5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5svg5,libqt5waylandcompositor5,libqt5widgets5,libstdc++6,mauikit-filebrowsing-git \(\>= 2.1.1+git\),mauikit-git \(\>= 2.1.1+git\),qml-module-qt-labs-calendar,qml-module-qt-labs-platform" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
